3348bbfc09b6ecadd2c29a68295cc86aad7e302e
msoderquist <msoderquist@mastercontrol.com>
2021-12-15 09:40:12 -0700

    Initial CI script and app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders DSR FlightLog text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/DSR FlightLog/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/DSR FlightLog/i);
+  expect(textElement).toBeInTheDocument();
 });
