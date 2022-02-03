40555af9d79b7fda504b61706364e1e35471a702
msoderquist <msoderquist@mastercontrol.com>
2022-02-02 18:01:34 -0700

    #14 Membership request

--- src/UserGroups.js
+++ src/UserGroups.js
@@ -1,6 +1,5 @@
 import "./css/member.css"
 import React, {useEffect, useState} from "react";
-import Loading from "./part/Loading";
 import NoResults from "./part/NoResults";
 import Icons from "./util/Icons";
 import Notice from "./part/Notice";
@@ -12,8 +11,8 @@ import GroupService from "./api/GroupService";
 import TokenService from "./api/TokenService";
 
 export default function UserGroups(props) {
+	const navigate = useNavigate();
 
-	const [page] = useState(0)
 	const [groups, setGroups] = useState([])
 	const [memberships, setMemberships] = useState([])
 	const [messages, setMessages] = useState([])
@@ -22,13 +21,14 @@ export default function UserGroups(props) {
 		setMessages([])
 	}
 
-	function loadGroupPage(page) {
-		UserService.getGroupPage(page, (response) => {
-			setGroups(response.groups)
+	function loadGroups() {
+		GroupService.getAvailableGroups((success) => {
+			setGroups(success)
 		}, (failure) => {
 			let messages = failure.messages
 			if (!!!messages) messages = [failure.message]
-			if (!!messages) setMessages(messages)
+			console.log(messages)
+			// if (!!messages) setMessages(messages)
 		})
 	}
 
@@ -52,22 +52,15 @@ export default function UserGroups(props) {
 		})
 	}
 
-	useEffect(() => loadGroupPage(page), [page])
+	useEffect(() => loadGroups(), [])
 	useEffect(() => loadMemberships(), [])
-
-	let list;
-	if (!!groups) {
-		list = <GroupList orgs={groups}/>
-	} else {
-		list = <Loading/>
-	}
 
 	return (
 		<div className='page-container'>
 			<div className='page-body'>
 				<div className='page-form'>
-					<MembershipList memberships={memberships} onMembershipRequest={requestMembership}/>
-					{list}
+					<MembershipList memberships={memberships} groups={groups} onMembershipRequest={requestMembership}/>
+					<button className='page-action' onClick={() => navigate(AppPath.GROUP + "/new")}>Create a new Group</button>
 					<Notice priority='error' messages={messages} clearMessages={clearMessages}/>
 				</div>
 			</div>
@@ -78,18 +71,7 @@ export default function UserGroups(props) {
 function JoinRequest(props) {
 
 	const [group, setGroup] = useState(props.group || '')
-	const [groupOptions, setGroupOptions] = useState(props.groupOptions || [])
 
-	function loadGroups() {
-		GroupService.getAvailableGroups((success) => {
-			setGroupOptions(success)
-		}, (failure) => {
-			let messages = failure.messages
-			if (!!!messages) messages = [failure.message]
-			console.log(messages)
-			// if (!!messages) setMessages(messages)
-		})
-	}
 
 	function requestMembership() {
 		props.onMembershipRequest(group)
@@ -97,16 +79,14 @@ function JoinRequest(props) {
 	}
 
 	function getContent() {
-		const hasOptions = !!groupOptions && groupOptions.length > 0
+		const hasOptions = !!props.groups && props.groups.length > 0
 
 		return hasOptions ?
 			<EntrySelect id='group' text='Group' value={group} required defaultValue='unspecified' onChange={(event) => setGroup(event.target.value)} fieldActionIcon={Icons.GROUP_ADD} onFieldAction={requestMembership}>
 				<option key='unspecified' hidden>Select a group</option>
-				{groupOptions.map((option) => <option key={option.id} value={option.id}>{option.name}</option>)}
+				{props.groups.map((option) => <option key={option.id} value={option.id}>{option.name}</option>)}
 			</EntrySelect> : <div>No groups available to join</div>
 	}
-
-	useEffect(() => loadGroups(), [])
 
 	return (
 		getContent()
@@ -114,32 +94,31 @@ function JoinRequest(props) {
 
 }
 
-function GroupList(props) {
-	const navigate = useNavigate();
-
-	let page
-	if (props.orgs.length === 0) {
-		page = <NoResults message='No groups owned by user'/>
-	} else {
-		page = props.orgs.map((group) => <GroupRow key={group.id} group={group}/>)
-	}
-
-	return (
-		<div className='vbox'>
-			<button className='page-action' onClick={() => navigate(AppPath.GROUP + "/new")}>Create a Group</button>
-			{page}
-		</div>
-	)
-}
-
-function GroupRow(props) {
-
-	const navigate = useNavigate();
-
-	return (
-		<div className='page-result' onClick={() => navigate(AppPath.GROUP + "/" + props.group.id)}>{Icons.fromGroupType(props.group.type)} {props.group.name}</div>
-	)
-}
+// function GroupList(props) {
+// 	const navigate = useNavigate();
+//
+// 	let page
+// 	if (props.orgs.length === 0) {
+// 		page = <NoResults message='No groups owned by user'/>
+// 	} else {
+// 		page = props.orgs.map((group) => <GroupRow key={group.id} group={group}/>)
+// 	}
+//
+// 	return (
+// 		<div className='vbox'>
+// 			{page}
+// 		</div>
+// 	)
+// }
+//
+// function GroupRow(props) {
+//
+// 	const navigate = useNavigate();
+//
+// 	return (
+// 		<div className='page-result' onClick={() => navigate(AppPath.GROUP + "/" + props.group.id)}>{Icons.fromGroupType(props.group.type)} {props.group.name}</div>
+// 	)
+// }
 
 function MembershipList(props) {
 	// Actions
@@ -149,18 +128,27 @@ function MembershipList(props) {
 		setJoinRequest(!joinRequest)
 	}
 
+	let memberships = <NoResults message='No group memberships'/>
+	if (!!props.memberships && props.memberships.length > 0) memberships = props.memberships.map((membership) => <MembershipRow key={membership.id} membership={membership}/>)
+
 	return (
 		<div className='vbox'>
 			<button className='page-action' onClick={() => toggleJoinRequest()}>Join a Group</button>
-			{joinRequest ? <JoinRequest onClose={toggleJoinRequest} onMembershipRequest={props.onMembershipRequest}/> : null}
-			{props.memberships.map((membership) => <MembershipRow key={membership.id} membership={membership}/>)}
+			{joinRequest ? <JoinRequest groups={props.groups} onClose={toggleJoinRequest} onMembershipRequest={props.onMembershipRequest}/> : null}
+			{memberships}
 		</div>
 	)
 }
 
 function MembershipRow(props) {
+	const navigate = useNavigate();
+
+	function doClick() {
+		if (props.membership.status === 'owner') navigate(AppPath.GROUP + "/" + props.membership.group.id)
+	}
+
 	return (
-		<div className='page-result'><MembershipStatus status={props.membership.status}/> {props.membership.group.name} </div>
+		<div className='page-result' onClick={doClick}><MembershipStatus status={props.membership.status}/> {Icons.fromGroupType(props.membership.group.type)} {props.membership.group.name} </div>
 	)
 }
 
