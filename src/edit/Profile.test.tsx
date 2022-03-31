0284f3bbf6325f5becebea010e1844238c4697bf
msoderquist <msoderquist@mastercontrol.com>
2022-03-31 11:00:51 -0600

    Update tests

--- src/edit/Profile.test.tsx
+++ src/edit/Profile.test.tsx
@@ -1,5 +1,5 @@
-import {render, screen} from "@testing-library/react";
 import React from "react";
+import {render, screen} from "@testing-library/react";
 import {MemoryRouter as Router} from "react-router-dom";
 import Profile from "./Profile";
 
