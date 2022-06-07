2c83eb0fd7eafcf564fa96c26a8a4b8e7b69d3cf
msoderquist <msoderquist@mastercontrol.com>
2022-06-07 11:42:01 -0600

    Move privacy info to about page

--- src/About.tsx
+++ src/About.tsx
@@ -13,25 +13,31 @@ export default function About(props) {
 		<div className='page-container'>
 			<div className='page-body'>
 				<div className='page-form'>
-					<div className='page-header-row'>
-						<h1>About</h1>
-						<span className='icon' onClick={close}>{Icons.CLOSE}</span>
-					</div>
-					<div><span className='page-label'>version</span> {props.version}</div>
-					<hr/>
-					<div><span className='page-label'>hosted by</span> <a href='https://www.desertskyrangers.org/'>Desert Sky Rangers</a></div>
-					<div><span className='page-label'>written by</span> Mark Soderquist</div>
-					<hr/>
-					<div><a href='https://github.com/desertskyrangers/flightdeck/issues/new'>Submit an enhancement request</a></div>
-					<div><a href='https://github.com/desertskyrangers/flightdeck/issues/new'>Submit a bug report</a></div>
-
-					<h1>Privacy Policy</h1>
-
 					<div>
-						Collection of personal information (name, email, phone number, etc.)
-						is for the express purpose to support FlightDeck members. Personal
-						information is not released to any other group or entity for any
-						purpose except if legally required.
+						<div className='page-header-row'>
+							<h1>About</h1>
+							<span className='icon' onClick={close}>{Icons.CLOSE}</span>
+						</div>
+						<div><span className='page-label'>version</span> {props.version}</div>
+						<hr/>
+						<div>
+							<span className='page-label'>hosted by</span> <a href='https://www.desertskyrangers.org/'>Desert Sky Rangers</a><br/>
+							<span className='page-label'>written by</span> Mark Soderquist
+						</div>
+						<hr/>
+						<div>
+							<a href='https://github.com/desertskyrangers/flightdeck/issues/new'>Submit an enhancement request</a><br/>
+							<a href='https://github.com/desertskyrangers/flightdeck/issues/new'>Submit a bug report</a>
+						</div>
+					</div>
+					<div>
+						<h1>Privacy Policy</h1>
+						<p>
+							Collection of personal information (name, email, phone number, etc.)
+							is for the express purpose to support FlightDeck members. Personal
+							information is not released to any other group or entity for any
+							purpose except if legally required.
+						</p>
 					</div>
 
 				</div>
