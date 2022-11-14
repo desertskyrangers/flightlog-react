f840a731c7ea75c504000bf21a8fc289395cc99e
msoderquist <msoderquist@mastercontrol.com>
2022-11-13 20:13:48 -0700

    Continue work on locations

---
+++ src/edit/Location.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import {MemoryRouter as Router} from "react-router";
+import Location from "./Location";
+
+test('renders name field', () => {
+	render(<Router><Location/></Router>);
+	const element = screen.getByLabelText(/name/i);
+	expect(element).toBeInTheDocument();
+	expect(element).toHaveAttribute('type', 'text');
+	expect(element).toHaveClass('page-field');
+});
+
